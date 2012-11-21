8b44536a9607cc8d8b29ee10650ffb294b83314c
Heesien Ooi <heesien.ooi@gmail.com>
2012-11-21 23:51:34 +1100

    Remove setinterval and use placeholder with fallback on Hiddenlabel

--- jquery-hidelabel.js
+++ jquery-hidelabel.js
@@ -1,39 +1,32 @@
 /* =============================================================
- * jquery-hidelabel.js v1
+ * jquery-hidelabel.js v1.1
  * https://github.com/Heesien/jquery-hidelabel.js
  * =============================================================
  * 
  * Author: Heesien Ooi < heesien.ooi@butterfly.com.au / heesien.ooi@gmail.com >
- * Requirement: jQuery 1.7+
+ * Requirement: jQuery
  *
  * Hide label for input, ie. text, password, textarea...
  * Basic usage: $('input').hidelabel();
  */
 
-!(function ($) {
+(function ($) {
 
 	"use strict";
 
+	// Detech placeholder support
+	var placeholderSupport = ('placeholder' in document.createElement('input'));
+
 	// HIDDENLABEL CLASS DEFINITION
+	// Construct when no placeholder support
 	var Hiddenlabel = function(element, label) {
 		this.$element = $(element);
-
-		switch (typeof label) {
-			case 'undefined':
-				this.$label = $('label[for="' + element.id +'"]');
-				break;
-			case 'object':
-				this.$label = $(label);
-				break;
-			default: // String, number, etc..
-				this.$label = $('<label for="' + element.id +'">' + label +'</label>');
-		}
-
+		this.$label = label;
+	
 		this.initialize();
-	}
+	};
 
 	Hiddenlabel.prototype = {
-
 		constructor: Hiddenlabel,
 
 		initialize: function() {
@@ -59,24 +52,41 @@
 
 				blur: function() {
 					$label.removeClass('el-focused');
-				}
-			})
+				},
 
-			setInterval(function() {
-				$label[($element.val() ? 'add' : 'remove') + 'Class']('el-populated');
-			}, 100);
+				'keydown keyup': function() {
+					$label[($element.val() ? 'add' : 'remove') + 'Class']('el-populated');
+				}
+			});
 		}
-	}
+	};
 
 	// HIDELABEL PLUGIN DEFINITION
 	$.fn.hidelabel = function(fn) {
 		return this.each(function() {
 			var $this = $(this),
-				data = $this.data('hiddenlabel'),
-				label = (typeof fn == 'function') ? fn.call(this, this) : undefined;
+				data = $this.data('hidelabel'),
+				label = (typeof fn === 'function') ? fn.call(this, this) :  undefined;
 
-			!data && $this.data('hiddenlabel', (data = new Hiddenlabel(this, label)));
-		})
-	}
+			// Check if it has hidelabel data
+			if (data) return;
+
+			// Create label object
+			switch (typeof label) {
+				case 'undefined':
+					label = $('label[for="' + this.id +'"]');
+					break;
+				case 'object':
+					label = $(label);
+					break;
+				default: // String, number, etc ..
+					label = $('<label for="' + this.id +'">' + label +'</label>');
+			}
+
+			// Add placeholder attribute if it supported or else fallback with Hiddenlabel
+			data = (placeholderSupport) ? $this.attr('placeholder', label.hide().text()) : new Hiddenlabel(this, label);
+			$this.data('hiddenlabel', data);
+		});
+	};
 
 })(jQuery);
